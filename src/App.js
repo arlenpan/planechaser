fc52d27cf353defb1856e937cc9718aa5c52af6f
Arlen Pan <arlenpan@gmail.com>
2019-02-25 11:46:14 -0800

    add deploy script, start on styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import cards from './data/cards.json';
 
 export default class App extends Component {
+    shuffle = a => {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
     render() {
+        const cardsShuffled = this.shuffle(Object.values(cards));
         return (
-            <div className="App">
-                Hello World! This is Planechaser.
-                <img src={`${process.env.PUBLIC_URL}/planes/planechase-anthology-promo/tazeem.jpg`} />;
+            <div className="main-container">
+                {cardsShuffled.map(c => (
+                    <img src={process.env.PUBLIC_URL + c.path} />
+                ))}
             </div>
         );
     }
 }
+
